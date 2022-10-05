34d6ea15659c6fa5ff35e86726f19c90bfb580c0
Rupesh Sreeraman <exmplayer.dev@gmail.com>
2022-10-05 21:26:56 +0530

    Headers sorted main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,10 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#include <QGuiApplication>
-#include <QApplication>
-#include <QQmlApplicationEngine>
 #include "texttoimagebackend.h"
-#include <QQuickStyle>
+#include <QApplication>
 #include <QDateTime>
+#include <QQmlApplicationEngine>
 
 bool bInit = true;
 
